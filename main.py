4d10048f4a66f067200dcf094d5c98d621815947
DocDrewToo <90295104+DocDrewToo@users.noreply.github.com>
2021-11-11 18:59:03 -0500

    Starting position added

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ total_visits_table = {}
 
 q_value_table = {}
 
+maze_positions = ("A1","A2","A3","A4","A5","A6","A7",
+                  "B1","B2","B3","B4","B5","B6","B7",
+                  "C1","C2","C3","C4","C5","C6","C7",
+                  "D1","D2","D3","D4","D5","D6","D7",
+                  "E1","E2","E3","E4","E5","E6","E7",
+                  "F1","F2","F3","F4","F5","F6","F7")
+
 reward_table = {
   "A1":-50,"A2":-50,"A3":-50,"A4":-50,"A5":-50,"A6":-50,"A7":-50,
   "B1":-50,"B2":0,"B3":0,"B4":0,"B5":0,"B6":"WALL","B7":-50,
@@ -33,6 +40,10 @@ directions = ["NORTH",
               "WEST"]
 
 
+def starting_position(maze):
+    return random.choice(maze)
+
+  
 def get_random_direction():
     return random.choice(directions)
 
