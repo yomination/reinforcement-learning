7ce9ead22f4d0b3228b355407d1857b87b8a7e9d
Doc Drew <docdrew@umich.edu>
2021-11-12 19:40:00 -0500

    calculation of q value verified!

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import random
 
+
+gamma = 0.1
+
 total_visits_table = {}
 
 q_value_table = {}
 
-maze_positions = ("A1","A2","A3","A4","A5","A6","A7",
-                  "B1","B2","B3","B4","B5","B6","B7",
-                  "C1","C2","C3","C4","C5","C6","C7",
-                  "D1","D2","D3","D4","D5","D6","D7",
-                  "E1","E2","E3","E4","E5","E6","E7",
-                  "F1","F2","F3","F4","F5","F6","F7")
-
-reward_table = {
+valid_starting_positions = ("B2","B3","B4","B5",
+                            "C3","C4","C6",
+                            "D3","D6",
+                            "E2","E3","E4","E5","E6",
+                            )
+intial_rewards_table = {
   "A1":-50,"A2":-50,"A3":-50,"A4":-50,"A5":-50,"A6":-50,"A7":-50,
   "B1":-50,"B2":0,"B3":0,"B4":0,"B5":0,"B6":"WALL","B7":-50,
   "C1":-50,"C2":"WALL","C3":0,"C4":0,"C5":"WALL","C6":0,"C7":-50,
@@ -20,34 +21,51 @@ reward_table = {
   "F1":-50,"F2":-50,"F3":-50,"F4":-50,"F5":-50,"F6":-50,"F7":-50,
 }
 
-cost_to_move = {
+reward_for_move = {
     "NORTH":-3,
     "SOUTH":-1,
     "EAST":-2,
     "WEST":-2
 }
 
-reward_to_move = {
-    "NORTH":3,
-    "SOUTH":1,
-    "EAST":2,
-    "WEST":2
-}
-
-directions = ["NORTH",
+directions = ("NORTH",
               "SOUTH",
               "EAST",
-              "WEST"]
+              "WEST")
 
 
-def starting_position(maze):
-    return random.choice(maze)
+def starting_position():
+    return random.choice(valid_starting_positions)
 
   
 def get_random_direction():
     return random.choice(directions)
 
-  
+def get_max_q_value(position):
+
+    q_values_all_directions = [
+        q_value_table.get(position+"_"+directions[0]),
+        q_value_table.get(position+"_"+directions[1]),
+        q_value_table.get(position+"_"+directions[2]),
+        q_value_table.get(position+"_"+directions[3])
+    ]
+    return max(q_values_all_directions)
+
+def calculate_q_value(current_position, current_direction, future_position, future_direction):
+    current_point = current_position+"_"+current_direction
+    future_point = future_position+"_"+future_direction
+
+    current_state_q = q_value_table.get(current_point)
+    
+    number_of_tries_ratio = 1 / (1 + total_visits_table.get(current_point))
+    current_action_reward = reward_for_move.get(current_direction)
+
+    max_q_value = get_max_q_value(future_position)
+
+    gamma_probability = current_state_q + number_of_tries_ratio * \
+         ( current_action_reward + gamma * \
+         max_q_value - current_state_q )
+    return gamma_probability
+
 if __name__ == "__main__":
-    for x in range(1,10):
-        print(x, get_random_direction())
+    print("Hello")
--- test.py
+++ test.py
@@ -14,11 +14,37 @@ class MainTests(unittest.TestCase):
         
     def test_ramdomily_entering_room_is_valid_room(self):
         message = "Initial random entry into 'maze' is a valid room"
-        list_of_valid_rooms_string = "".join(main.maze_positions)
-        starting_position = main.starting_position(main.maze_positions)     
+        list_of_valid_rooms_string = "".join(main.valid_starting_positions)
+        starting_position = main.starting_position()
 
         self.assertIn(starting_position, list_of_valid_rooms_string, message)
 
+
+    def test_get_max_q_value(self):
+        position_in_maze = "B2"
+        main.q_value_table = {"B2_NORTH":5,
+                              "B2_SOUTH":1,
+                              "B2_EAST":1,
+                              "B2_WEST":1,
+        }
+        expected_max_q_value = 5
+        actual_max_q_value = main.get_max_q_value(position_in_maze)
+
+        self.assertEqual(expected_max_q_value, actual_max_q_value)
+
+    def test_calculated_q_value(self):
+        current_position = "B2"
+        current_direction = "EAST"
+        future_position = "B3"
+        future_direction = "SOUTH"
+        main.total_visits_table = {"B2_EAST":9}
+        main.q_value_table = {"B2_EAST":5,"B3_NORTH":2,"B3_EAST":2,"B3_SOUTH":50,"B3_WEST":2}
         
+        explected_q_value = 4.8
+        actual_q_value = main.calculate_q_value(current_position,
+        current_direction, future_position, future_direction)
+
+        self.assertEqual(explected_q_value, actual_q_value)
+
 if __name__ == '__main__':
     unittest.main()
